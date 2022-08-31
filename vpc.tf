3dd0012e6f356e0a52a3027395fce80ec12de057
David Clark <dclar1@amfam.com>
2022-08-31 19:12:54 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,9 @@
 module "network" {
   source  = "app.terraform.io/DavidClark-training/network/google"
-  version = "2.5.0"
+  version = "3.4.0"
   network_name = "gaurav-network"
   project_id = var.project
   subnets = [
-subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
